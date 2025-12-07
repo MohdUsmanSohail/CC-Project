d304f59f458cd91e8e884accb5134bda75fbdeb4
Usman-Sohail <mohd.usman.sohail@gmail.com>
2025-12-07 19:14:21 +0500

    Create: lexer/__init__.py

---
+++ src/lexer/__init__.py
@@ -0,0 +1,18 @@
+from .tokens import (
+    KEYWORDS,
+    SINGLE_CHARS,
+    OPERATORS,
+    Token
+)
+
+from .lexer import (
+    Lexer
+)
+
+__all__ = [
+    "KEYWORDS",
+    "SINGLE_CHARS",
+    "OPERATORS",
+    "Token",
+    "Lexer"
+]
